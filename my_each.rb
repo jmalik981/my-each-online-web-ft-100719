771fc6f1ecf86e99433eb7b9299e7087f807fa2b
Junaid Malik <jmalik981@gmail.com>
2019-10-22 19:11:21 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,13 @@
-def my_each # put argument(s) here
-  # code here
-my_each(words) do |i|
-  puts i
+# def my_each # put argument(s) here
+#   # code here
+
+# end
+
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
   end
+  collection
 end
